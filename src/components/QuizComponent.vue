5b5108c612f08706d9754bb95b78fb1c09a60732
Mio Arai <mio311a@gmail.com>
2023-11-02 15:28:48 -0400

    correct incorrect

--- src/components/QuizComponent.vue
+++ src/components/QuizComponent.vue
@@ -1,85 +1,83 @@
 <template>
-    <div class="questions">
-      <h1>Question No.{{ currentQuestion.id }}</h1>
-      <div class="question" v-if="currentQuestion">
-        <h2>{{ currentQuestion.question }}</h2>
-        <img :src="require('@/assets/' + currentQuestion.imageUrl)" alt="Question Image" />
-        <div v-for="(option, index) in currentQuestion.options" :key="index">
-          <input
-            type="radio"
-            :id="'option_' + index"
-            :value="index"
-            v-model="selectedAnswer"
-          />
-          <label :for="'option_' + index">{{ option }}</label>
-        </div>
-        <button @click="submitAnswer">Submit</button>
+  <div class="questions">
+    <h1>Question No.{{ currentQuestion.id }}</h1>
+    <div class="question" v-if="currentQuestion">
+      <h2>{{ currentQuestion.question }}</h2>
+      <img
+        :src="require('@/assets/' + currentQuestion.imageUrl)"
+        alt="Question Image"
+      />
+      <div v-for="(option, index) in currentQuestion.options" :key="index">
+        <input
+          type="radio"
+          :id="'option_' + index"
+          :value="index"
+          v-model="selectedAnswer"
+        />
+        <label :for="'option_' + index">{{ option }}</label>
       </div>
-      <div v-else>
-        <p>Congratulations! You have completed the quiz.</p>
-      </div>
-      <button v-if="showNextButton" @click="nextQuestion">Go to Next Question</button>
+      <button @click="submitAnswer">Submit</button>
+      <p :style="{ color: feedbackColor }">{{ feedbackMessage }}</p>
     </div>
-  </template>
+    <div v-else>
+      <p>Congratulations! You have completed the quiz.</p>
+    </div>
+    <button v-if="showNextButton" @click="nextQuestion">
+      Go to Next Question
+    </button>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: 'quiz-questions',
-    data() {
-      return {
-        currentQuestionIndex: 0,
-        selectedAnswer: null,
-      };
+export default {
+  name: "quiz-questions",
+  data() {
+    return {
+      currentQuestionIndex: 0,
+      selectedAnswer: null,
+      feedbackColor: "black", // Default color
+      feedbackMessage: "",
+    };
+  },
+  computed: {
+    questions() {
+      return this.$store.state.questions;
     },
-    computed: {
-      questions() {
-        return this.$store.state.questions;
-      },
-      currentQuestion() {
-        return this.questions[this.currentQuestionIndex];
-      },
-      showNextButton() {
-        return this.currentQuestionIndex < this.questions.length - 1;
-      },
+    currentQuestion() {
+      return this.questions[this.currentQuestionIndex];
     },
-    methods: {
-        submitAnswer() {
-    if (this.selectedAnswer !== null) {
-      console.log('Selected Answer:', this.selectedAnswer);
-      console.log('Correct Answer:', this.currentQuestion.answer);
-      
-      const isCorrect = this.selectedAnswer === this.currentQuestion.answer;
-      console.log('Is Correct:', isCorrect);
+    showNextButton() {
+      return this.currentQuestionIndex < this.questions.length - 1;
+    },
+  },
+  methods: {
+    submitAnswer() {
+      if (this.selectedAnswer !== null) {
+        const selectedAnswerInt = parseInt(this.selectedAnswer, 10);
+        const isCorrect = selectedAnswerInt === this.currentQuestion.answer;
 
-      if (isCorrect) {
-        this.showAnswerFeedback(true);
-      } else {
-        this.showAnswerFeedback(false);
+        if (isCorrect) {
+          this.setFeedback("Correct! Good job.", "green");
+        } else {
+          this.setFeedback("Oops, that was incorrect. Try again!", "red");
+        }
       }
-    }
-},
-  showAnswerFeedback(isCorrect) {
-    // You can define your feedback logic here.
-    if (isCorrect) {
-      // For a correct answer
-      alert('Correct! Good job.');
-      // You can also update the UI or store the result.
-    } else {
-      // For an incorrect answer
-      alert('Oops, that was incorrect. Try again!');
-      // You can also provide hints or additional feedback.
-    }
+    },
+    setFeedback(message, color) {
+      this.feedbackMessage = message;
+      this.feedbackColor = color;
+    },
+    nextQuestion() {
+      this.selectedAnswer = null;
+      this.currentQuestionIndex++;
+      this.feedbackMessage = ""; // Clear feedback when moving to the next question
+      this.feedbackColor = "black"; // Reset the feedback color
+    },
   },
-  nextQuestion() {
-    this.selectedAnswer = null;
-    this.currentQuestionIndex++;
-  },
-},
-
-  };
-  </script>
+};
+</script>
   
   <style>
-  /* Add your CSS styling here if needed */
-  </style>
+/* Add your CSS styling here if needed */
+</style>
   
